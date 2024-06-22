3c9f2f125ead0e4e1208b4a90cb429517348ddbd
babel984 <57294544+babel984@users.noreply.github.com>
2024-06-22 14:19:50 +0100

    first commit

---
+++ polls/urls.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.urls import path, include
+from. import views
+
+
+urlpatterns = [
+    path('', views.Index,name='Index'),
+    path('about', views.About,name='About'),
+    path('contact', views.Contact,name='Contact'),
+    path('mission&vission', views.Mission,name='Mission'),
+    path('powers&functions', views.Powersfunctions,name='Powersfunctions'),
+    path('project', views.Project,name='Project'),
+    path('team', views.Team,name='Team'),
+    path('testimonial', views.Testimonial,name='Testimonial'),
+    path('service', views.Service,name='Service'),
+    path('services', views.Services,name='Services'),
+    path('demographic-map', views.Demographic,name='Demographic'),
+    path('chairman', views.Chairman,name='Chairman'),
+    path('clerk', views.Clerk,name='Clerk'),
+    path('ceo', views.Ceo,name='Ceo'),
+    path('council', views.Council,name='Council'),
+    path('Planning&development', views.Planningdevelopment,name='Planningdevelopment'),
+    path('council-members', views.Council,name='Council'),
+    path('Budget&finance', views.Budgetfinance,name='Budgetfinance'),
+    path('lands', views.Lands,name='Lands'),
+    path('sanitation', views.Sanitation,name='Sanitation'),
+    path('education', views.Education,name='Education'),
+    path('health', views.Health,name='Health'),
+    path('community', views.Community,name='Community'),
+    path('agriculture', views.Agriculture,name='Agriculture'),
+    path('publication', views.Publications,name='Publications'),
+    path('partners', views.Partners,name='Partners'),
+    path('int-partners', views.Intpartners,name='Intpartners'),
+    path('rates&taxes', views.Ratestaxes,name='Ratestaxes'),
+    
+    ]
+#urlpatterns += static(settings.MEDIA.URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.STATIC.URL, document_root=settings.STATIC_ROOT)
+
+
+
---
+++ polls/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Index(request):
+    return render(request, 'index.html')
+
+def About(request):
+    return render(request, 'about.html')
+
+def Service(request):
+    return render(request, 'service.html')
+
+def Services(request):
+    return render(request, 'services.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
+
+def Project(request):
+    return render(request, 'project.html')
+
+def Testimonial(request):
+    return render(request, 'testimonial.html')
+    
+def Sanitation(request):
+    return render(request, 'sanitatione.html')
+
+def Education(request):
+    return render(request, 'education.html')
+
+def Team(request):
+    return render(request, 'team.html')
+
+def Error(request):
+    return render(request, '404.html')
+
+def Health(request):
+    return render(request, 'health.html')
+
+def Community(request):
+    return render(request, 'community.html')
+
+def Agriculture(request):
+    return render(request, 'agriculture.html')
+
+def Community(request):
+    return render(request, 'community.html')
+
+def Lands(request):
+    return render(request, 'lands.html')
+
+def Mission(request):
+    return render(request, 'mission&vission.html')
+
+def Powersfunctions(request):
+    return render(request, 'Powers&functions.html')
+
+def Demographic(request):
+    return render(request, 'Demographic-map.html')
+
+def Chairman(request):
+    return render(request, 'chairman.html')
+
+def Clerk(request):
+    return render(request, 'clerk.html')
+
+def Ceo(request):
+    return render(request, 'ceo.html')
+
+def Planningdevelopment(request):
+    return render(request, 'Planning&development.html')
+
+def Council(request):
+    return render(request, 'council-members.html')
+
+def Ratestaxes(request):
+    return render(request, 'rates&taxes.html')
+
+def Budgetfinance(request):
+    return render(request, 'Budget&finance.html')
+
+def Partners(request):
+    return render(request, 'partners.html')
+
+def Intpartners(request):
+    return render(request, 'int-partners.html')
+
+def Publications(request):
+    return render(request, 'publication.html')
+
+def Sanitation(request):
+    return render(request, 'sanitation.html')
+
+
+
+
+
